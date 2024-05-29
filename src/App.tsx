92f016469a62c188991f777d656474dbb2e9aa52
Jack42chj <hojinch99@gmail.com>
2024-05-29 17:42:31 +0900

    GA script add

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { Suspense, lazy } from "react";
 import Loading from "./components/loading";
-import ReactGA from "react-ga";
 
 const Home = lazy(() => import("./pages/Home"));
 
@@ -44,10 +43,6 @@ const Wrapper = styled.div`
     height: 100vh;
     background-color: #ffffff;
 `;
-
-const gaTrackId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
-ReactGA.initialize(gaTrackId, { debug: true });
-ReactGA.pageview(window.location.pathname);
 
 const App = () => {
     return (
